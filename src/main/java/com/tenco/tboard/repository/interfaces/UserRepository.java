89f85acaa2782a6b924d072622977755425f6c5a
CYJ <wjdtpdbs1887@naver.com>
2024-07-15 11:48:45 +0900

    user에 대한dao 설계

---
+++ src/main/java/com/tenco/tboard/repository/interfaces/UserRepository.java
@@ -0,0 +1,15 @@
+package com.tenco.tboard.repository.interfaces;
+
+import java.util.List;
+
+import com.tenco.tboard.model.User;
+
+public interface UserRepository {
+	
+	void addUser(User user);
+	void deletUser(int id);
+	User getUserByusername(String username);
+	User getUserByusernameAndPassword(String username, String password);
+	List<User> getAllUsers();
+	
+}
